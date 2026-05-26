da42879ca18cf67556055224ea55ae1d1d99a33b
Jordan Abraham <personal.jordanabraham@gmail.com>
2026-05-26 10:57:46 -0400

    fix: bundle LSP binary and auto-start on project open
      Remove GitHub auto-download, require binary bundled in plugin resources.
      Start LSP eagerly via LspServerManager on project open.
      Require IDE restart on plugin install.

--- plugin/src/main/kotlin/rs2/lsp/Rs2StartupActivity.kt
+++ plugin/src/main/kotlin/rs2/lsp/Rs2StartupActivity.kt
@@ -2,9 +2,12 @@ package rs2.lsp
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
+import com.intellij.platform.lsp.api.LspServerManager
 
 class Rs2StartupActivity : ProjectActivity {
     override suspend fun execute(project: Project) {
-        Rs2LspBinaryManager.checkForUpdatesAsync()
+        Rs2LspBinaryManager.getOrExtractBinary()
+        LspServerManager.getInstance(project)
+            .stopAndRestartIfNeeded(Rs2LspServerSupportProvider::class.java)
     }
 }
